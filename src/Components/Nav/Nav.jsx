a1c1bf3a70b8622bfdc2d5655c78edbcab661d95
Roccay <roccayzhou@gmail.com>
2022-08-15 16:50:21 -0400

    merge signup/signin and post models

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginForm from "../LoginForm/LoginForm";
+import SignUpForm from "../SignUpForm/SignUpForm";
 import MenuButton from "../MenuButton/MenuButton";
-
 function Nav(props) {
+  const [showLogin, setShowLogin] = useState({
+    showLogin: false,
+  });
+
+  const [showSignUp, setShowSignUp] = useState({
+    showSignUp: false,
+  });
+
   return (
     <div className="nav">
       <p className="Logo">Brand</p>
@@ -10,14 +19,33 @@ function Nav(props) {
         <p>Toronto, ON</p>
       </div>
       <button className="SearchButton">Search</button>
-      <button className="UploadSpotButton">Upload a Spot</button>
+      <p className="UploadSpotButton">Upload a Spot</p>
       <MenuButton
         name="Upload a Spot"
         styleRef="UploadSpotButton"
         route="/createPost"
       />
-      <button className="LoginButton">Log In</button>
-      <button className="SignUpButton">Sign Up</button>
+      <button
+        className="LoginButton"
+        onClick={() => {
+          setShowLogin({ showLogin: true });
+        }}
+      >
+        Log In
+      </button>
+      <button
+        className="SignUpButton"
+        onClick={() => {
+          setShowSignUp({ showSignUp: true });
+        }}
+      >
+        Sign Up
+      </button>
+      <LoginForm showLogin={showLogin.showLogin} setShowLogin={setShowLogin} />
+      <SignUpForm
+        showSignUp={showSignUp.showSignUp}
+        setShowSignUp={setShowSignUp}
+      />
     </div>
   );
 }
