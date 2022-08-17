1501bbd87ec8e34c7450a578e752db6b5457bfb0
Roccay <roccayzhou@gmail.com>
2022-08-17 18:47:49 -0400

    change post model, now connect to user obj

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import LoginForm from "../LoginForm/LoginForm";
 import SignUpForm from "../SignUpForm/SignUpForm";
 import MenuButton from "../MenuButton/MenuButton";
-import "./Nav.css"
+import "./Nav.css";
 
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
@@ -15,67 +15,76 @@ function Nav(props) {
   const [showSignUp, setShowSignUp] = useState({
     showSignUp: false,
   });
-  
-    const [showSignUp, setShowSignUp] = useState({
-        showSignUp: false,
-    });
 
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        props.setUserInState(null);
-        setShowSignUp({ showSignUp: false });
-        setShowLogin({ showLogin: false });
-    };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    props.setUserInState(null);
+    setShowSignUp({ showSignUp: false });
+    setShowLogin({ showLogin: false });
+  };
 
-    return (
-        <div className="nav">
-            <div className="logo-image">
-                <img src="../../Logo-Black.png" alt="" />
-            </div>
-            <SearchBar
-                searchQuery={props.searchQuery}
-                setSearchQuery={props.setSearchQuery}
-            />
-            <MenuButton
-                name="Upload a Spot"
-                styleRef="UploadSpotButton"
-                route="/createPost"
-            />
-            <div style={{ display: props.user ? "none" : "flex" }}>
-                <button
-                    className="LoginButton"
-                    onClick={() => {
-                        setShowLogin({ showLogin: true });
-                    }}
-                >
-                    Log In
-                </button>
-                <button
-                    className="SignUpButton"
-                    onClick={() => {
-                        setShowSignUp({ showSignUp: true });
-                    }}
-                >
-                    Sign Up
-                </button>
-                <LoginForm showLogin={showLogin.showLogin} setShowLogin={setShowLogin} setUserInState={props.setUserInState} user={props.user} />
-                <SignUpForm
-                    showSignUp={showSignUp.showSignUp}
-                    setShowSignUp={setShowSignUp}
-                    setUserInState={props.setUserInState} user={props.user}
-                /> </div>
+  return (
+    <div className="nav">
+      <div className="logo-image">
+        <img src="../../Logo-Black.png" alt="" />
+      </div>
+      <SearchBar
+        searchQuery={props.searchQuery}
+        setSearchQuery={props.setSearchQuery}
+      />
+      <MenuButton
+        name="Upload a Spot"
+        styleRef="UploadSpotButton"
+        route="/createPost"
+      />
+      <div style={{ display: props.user ? "none" : "flex" }}>
+        <button
+          className="LoginButton"
+          onClick={() => {
+            setShowLogin({ showLogin: true });
+          }}
+        >
+          Log In
+        </button>
+        <button
+          className="SignUpButton"
+          onClick={() => {
+            setShowSignUp({ showSignUp: true });
+          }}
+        >
+          Sign Up
+        </button>
+        <LoginForm
+          showLogin={showLogin.showLogin}
+          setShowLogin={setShowLogin}
+          setUserInState={props.setUserInState}
+          user={props.user}
+        />
+        <SignUpForm
+          showSignUp={showSignUp.showSignUp}
+          setShowSignUp={setShowSignUp}
+          setUserInState={props.setUserInState}
+          user={props.user}
+        />{" "}
+      </div>
 
-            <div className="user-loggedin-container" style={{ display: props.user ? "flex" : "none" }}>
-                <div className="user-loggedin"><span>Logged as</span> <span className="user-loggedin-span">{props.user && props.user.name}</span></div>
-                <MenuButton name="My Profile" styleRef="" route="/profile" />
-                <button
-                    className="SignOutButton"
-                    onClick={handleLogout}
-                >Sign Out</button>
-
-            </div>
+      <div
+        className="user-loggedin-container"
+        style={{ display: props.user ? "flex" : "none" }}
+      >
+        <div className="user-loggedin">
+          <span>Logged as</span>{" "}
+          <span className="user-loggedin-span">
+            {props.user && props.user.name}
+          </span>
         </div>
-    );
+        <MenuButton name="My Profile" styleRef="" route="/profile" />
+        <button className="SignOutButton" onClick={handleLogout}>
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
