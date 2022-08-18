3111b7bbb1af8bc5e04e01ad8faad894cd239f90
Roccay <roccayzhou@gmail.com>
2022-08-18 18:25:20 -0400

    deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
 });
